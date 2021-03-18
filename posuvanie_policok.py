6e1afff2e68f1b8c85d5fd8cae9b30f0aef3cd0e
patriksimurka <patriksimurka@gmail.com>
2021-03-18 19:06:04 +0100

    drawing squares

--- posuvanie_policok.py
+++ posuvanie_policok.py
@@ -1,10 +1,15 @@
 import tkinter
 
-width, height = 400
-pad
+width = height = 500
+a = 100
+pad = 50
 canvas = tkinter.Canvas(width=width, height=height)
 canvas.pack()
+cisla = ['7', '1', '', '4', '13', '9', '3', '2', '14', '11', '12', '6', '10', '15', '8', '5']
 
-
-
+for i in range(len(cisla)):
+	if cisla[i] == '':
+		canvas.create_rectangle(pad+a*(i%4), pad+(i//4)*a+a, pad+a*(i%4)+a, pad+(i//4)*a, fill='orange')
+	else:
+		canvas.create_rectangle(pad+a*(i%4), pad+(i//4)*a+a, pad+a*(i%4)+a, pad+(i//4)*a, fill='grey')
 canvas.mainloop()
