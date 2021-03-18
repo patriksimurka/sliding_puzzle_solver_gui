6c134d9bd5cd7982e1fa6f2835fc104a33eff293
patriksimurka <patriksimurka@gmail.com>
2021-03-18 21:14:36 +0100

    rects with numbers

--- posuvanie_policok.py
+++ posuvanie_policok.py
@@ -6,10 +6,13 @@ pad = 50
 canvas = tkinter.Canvas(width=width, height=height)
 canvas.pack()
 cisla = ['7', '1', '', '4', '13', '9', '3', '2', '14', '11', '12', '6', '10', '15', '8', '5']
+boxes = []
 
 for i in range(len(cisla)):
 	if cisla[i] == '':
-		canvas.create_rectangle(pad+a*(i%4), pad+(i//4)*a+a, pad+a*(i%4)+a, pad+(i//4)*a, fill='orange')
+		box = canvas.create_rectangle(pad+a*(i%4), pad+(i//4)*a+a, pad+a*(i%4)+a, pad+(i//4)*a, fill='orange')
 	else:
-		canvas.create_rectangle(pad+a*(i%4), pad+(i//4)*a+a, pad+a*(i%4)+a, pad+(i//4)*a, fill='grey')
+		box = canvas.create_rectangle(pad+a*(i%4), pad+(i//4)*a+a, pad+a*(i%4)+a, pad+(i//4)*a, fill='grey')
+	boxes.append(box)
+	canvas.create_text((pad+a*(i%4)+pad+a*(i%4)+a)//2, (pad+(i//4)*a+a+pad+(i//4)*a)//2, text=cisla[i], font='Arial 15')
 canvas.mainloop()
